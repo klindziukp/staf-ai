43f5a8f25160ecdcd1c0ede6cd663739081172e1
Pavel Klindziuk <37264029+klindziukp@users.noreply.github.com>
2025-12-05 12:10:32 +0300

    Create src/main/java/com/klindziuk/staf/model/SearchRequest.java

---
+++ src/main/java/com/klindziuk/staf/model/SearchRequest.java
@@ -0,0 +1,25 @@
+package com.klindziuk.staf.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Model representing a search request for dataset records.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchRequest {
+
+    @Builder.Default
+    private String criteria = "*:*";
+
+    @Builder.Default
+    private Integer start = 0;
+
+    @Builder.Default
+    private Integer rows = 100;
+}
