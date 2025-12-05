134e5bdb16290724c01a6058fb7842a24c87010d
Pavel Klindziuk <37264029+klindziukp@users.noreply.github.com>
2025-12-05 11:07:50 +0300

    Create src/test/java/com/uspto/api/listeners/TestSuiteListener.java

---
+++ src/test/java/com/uspto/api/listeners/TestSuiteListener.java
@@ -0,0 +1,29 @@
+package com.uspto.api.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+/**
+ * TestNG suite listener for test suite level events.
+ * 
+ * @author USPTO API Test Team
+ * @version 1.0
+ */
+@Slf4j
+public class TestSuiteListener implements ISuiteListener {
+
+    @Override
+    public void onStart(ISuite suite) {
+        log.info("========================================");
+        log.info("Starting Test Suite: {}", suite.getName());
+        log.info("========================================");
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        log.info("========================================");
+        log.info("Finished Test Suite: {}", suite.getName());
+        log.info("========================================");
+    }
+}
