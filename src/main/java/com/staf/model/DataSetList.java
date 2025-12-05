92338a15e8b463fc300f6364eb9ec131f284daab
Pavel Klindziuk <37264029+klindziukp@users.noreply.github.com>
2025-12-05 12:50:25 +0300

    Create src/main/java/com/staf/model/DataSetList.java

---
+++ src/main/java/com/staf/model/DataSetList.java
@@ -0,0 +1,25 @@
+package com.staf.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Model representing the list of available data sets.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataSetList {
+    
+    @JsonProperty("total")
+    private Integer total;
+    
+    @JsonProperty("apis")
+    private List<ApiInfo> apis;
+}
