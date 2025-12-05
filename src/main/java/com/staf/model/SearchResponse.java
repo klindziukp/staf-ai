cba3dc04886e24224b09c7ef652a5e97b79694f4
Pavel Klindziuk <37264029+klindziukp@users.noreply.github.com>
2025-12-05 12:50:29 +0300

    Create src/main/java/com/staf/model/SearchResponse.java

---
+++ src/main/java/com/staf/model/SearchResponse.java
@@ -0,0 +1,29 @@
+package com.staf.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Model representing a search response from dataset records.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchResponse {
+    
+    @JsonProperty("numFound")
+    private Integer numFound;
+    
+    @JsonProperty("start")
+    private Integer start;
+    
+    @JsonProperty("docs")
+    private List<Map<String, Object>> docs;
+}
